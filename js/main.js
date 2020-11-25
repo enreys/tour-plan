c001d97acb4caf7eef93094d4f83f96fe2a1609a
Ilya <enreys@bk.ru>
2020-11-25 21:50:10 +0500

    create: slider

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    navigation: {
+      nextEl: '.slider-button--next',
+      prevEl: '.slider-button--prev',
+    },
+  });
