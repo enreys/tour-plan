1b6e027c4186835f775dedc26a7e98ee4b98bbf1
Ilya <enreys@bk.ru>
2020-12-08 00:16:08 +0500

    create: 2 modal windows

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var hotelSlider = new Swiper('.hotel-slider', {
+$(document).ready(function(){
+  var hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
     keyboard: {
       enabled: true,
@@ -63,5 +64,23 @@ $('.parallax-window').parallax({imageSrc: 'img/newsletter-bg.jpg'});
 var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener('click', function() {
   console.log('Клик по кнопке меню');
-  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
-} )
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible")});
+
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
+});
